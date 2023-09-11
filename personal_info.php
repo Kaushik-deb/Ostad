66fb5dc45990adbb70d504a692458f573882d81a
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-12 01:49:30 +0600

    new code

---
+++ personal_info.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "kaushik deb";
+    $age = "28";
+    $country = "bangladesh";
+    $short_Description = "Hi, my name is Kaushik Deb. I am trying to be an excelent laravel developer.";
+    echo ($name);
+    echo "\n";
+    echo ($age);
+    echo "\n";
+    echo ($country);
+    echo "\n";
+    echo ($short_Description);
+
+?>
