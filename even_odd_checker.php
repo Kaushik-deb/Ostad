f982bdb799a00f8c67655d4d73fe61569997daa0
kaushik deb <kaushikdeb2k12kuet@gmail.com>
2023-09-13 00:08:52 +0600

    new

---
+++ even_odd_checker.php
@@ -0,0 +1,13 @@
+<?php
+$num = 11;
+if($num==0){
+    echo "Sorry!! we have to check even or odd number to get full marks from ostad";
+}
+elseif($num%2==0){
+    echo "That's an even number";
+}
+else{
+    echo "That's an odd number";
+}
+
+?>
